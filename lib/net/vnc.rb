a78170553daf730b14ac1b3d8e1ae771754e946f
Alban Peignier <alban@tryphon.eu>
2014-05-29 15:11:14 +0200

    Fixes get_key_code for Ruby >= 1.9 string

--- lib/net/vnc.rb
+++ lib/net/vnc.rb
@@ -182,7 +182,7 @@ module Net
 				if which.length != 1
 					raise ArgumentError, 'can only get key_code of single character strings'
 				end
-				which[0]
+        which.bytes.first
 			else
 				KEY_MAP[which]
 			end
